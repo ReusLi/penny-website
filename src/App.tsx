4fd1fba16bc2d9e851d5f33974292c921e713886
ReusLi <574399934@qq.com>
2018-12-27 23:38:16 +0800

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import * as React from 'react'
 
-import Matrix from 'components/matrix/matrix'
-
-import AntdTable from 'components/antd-table'
-
 import { Row, Col } from 'antd'
 
 export default class App extends React.Component {
 
     public render() {
         return (
-            <Row gutter={20}>
-                <Col span={12}>
-                    <Matrix />
-                </Col>
-                <Col span={12}>
-                    <AntdTable />
-                </Col>
+            <Row>
+                <Row className={"top"}
+                    type="flex"
+                    justify="space-between"
+                    align="middle"
+                >
+                    <Col className={"top-col"}
+                        offset={1}
+                    >
+                        小宝2018日记
+                    </Col>
+
+                    <Col span={4}>
+                        <Col className={"top-col"} span={8}>照片</Col>
+                        <Col className={"top-col"} span={8}>日记</Col>
+                        <Col className={"top-col"} span={8}>我</Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <img src="../images/pitrue.jpg"></img>
+                    </Col>
+                </Row>
+                <Row></Row>
             </Row>
         )
     }
