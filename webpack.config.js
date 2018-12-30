280873673929ff439c0e6724888827bcbe299c53
ReusLi <574399934@qq.com>
2018-12-30 10:45:36 +0800

    add historyApiFallback for wp config for rc router

--- webpack.config.js
+++ webpack.config.js
@@ -99,11 +99,13 @@ module.exports = {
         })
         // new MonacoWebpackPlugin(webpack)
     ],
+    devServer: {
+        historyApiFallback: true
+    },
     resolve: {
         alias: {
             'pages': path.resolve(__dirname, 'src/pages'),
             'components': path.resolve(__dirname, 'src/components'),
-            'context': path.resolve(__dirname, 'src/context'),
             'interface': path.resolve(__dirname, 'src/interface'),
             'utils': path.resolve(__dirname, 'src/utils'),
             'store': path.resolve(__dirname, 'src/store')
