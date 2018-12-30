08ad2d0128b5502e392784d7faf761c62677d695
ReusLi <574399934@qq.com>
2018-12-30 23:22:21 +0800

    add diaryDate component

--- src/router.tsx
+++ src/router.tsx
@@ -3,23 +3,27 @@ import * as React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Row } from 'antd';
 
+import NavBar from 'pages/navBar'
+
 import Home from 'pages/home'
 import DiaryDate from 'pages/diaryDate'
+import DiaryList from 'pages/diaryList'
 
 import PyEditor from 'components/editor'
 
-const Users = () => <h2>Users</h2>;
-
 const topRow = {
-  height: '100%', 
+  height: '100%',
 }
 const AppRouter = () => (
   <Router>
-    <Row style={topRow} type="flex" justify="center">
-      <Route path="/" exact component={Home} />
-      <Route path="/pic/" component={DiaryDate} />
-      <Route path="/note/" component={PyEditor} />
-      <Route path="/users/" component={Users} />
+    <Row style={topRow}>
+      <NavBar />
+      <Row style={{ height: '95%', background: 'rgba(255, 255, 255, 0.3)' }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/pic/" component={DiaryDate} />
+        <Route path="/note/" component={PyEditor} />
+        <Route path="/user/" component={DiaryList} />
+      </Row>
     </Row>
   </Router>
 );
