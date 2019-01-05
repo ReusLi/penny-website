7a4d8ba69a300ed1e5e6f916fb6574a628a37d1f
ReusLi <574399934@qq.com>
2019-01-06 03:30:59 +0800

    add FroalaEditor witch react pattern of wysiwyg

---
+++ src/components/froalaEditor/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// Require Editor JS files.
+import 'froala-editor/js/froala_editor.pkgd.min.js';
+
+// Require Editor CSS files.
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
+// Require Font Awesome.
+import 'font-awesome/css/font-awesome.css';
+
+import FroalaEditor from 'react-froala-wysiwyg';
+
+// Include special components if required.
+// import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
+// import FroalaEditorA from 'react-froala-wysiwyg/FroalaEditorA';
+// import FroalaEditorButton from 'react-froala-wysiwyg/FroalaEditorButton';
+// import FroalaEditorImg from 'react-froala-wysiwyg/FroalaEditorImg';
+// import FroalaEditorInput from 'react-froala-wysiwyg/FroalaEditorInput';
+
+// Render Froala Editor component.
+export default class FroalaEditor extends React.Component<{}, {}> {
+    render() {
+        return (
+            <span>test</span>
+        )
+    }
+}
