a731eb54dc5cb4ea567fdead1f07f8322ffaea4b
ReusLi <574399934@qq.com>
2019-01-06 16:45:27 +0800

    udpate entity

--- backend/models/entity/py_diary.js
+++ backend/models/entity/py_diary.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
         id: { type: DataTypes.STRING(36), primaryKey: true, allowNull: false, comment: 'uuid' },
         userId: { type: DataTypes.STRING(36), allowNull: false, comment: '用户id' },
         title: { type: DataTypes.STRING(100), allowNull: false, comment: '日记标题' },
+        desc: { type: DataTypes.STRING(200), allowNull: true, comment: '日记的描述' },
         content: { type: DataTypes.TEXT, allowNull: true, comment: '日记内容' },
         createTime: { type: DataTypes.DATE, allowNull: false, comment: '创建时间' },
         updateTime: { type: DataTypes.DATE, allowNull: false, comment: '最后一次更新时间' }
