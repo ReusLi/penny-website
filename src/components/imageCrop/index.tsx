bee2aceb47e8fa7dccf4361783dae7895ae1f66f
ReusLi <574399934@qq.com>
2019-01-06 16:45:17 +0800

    add image crop explame

--- src/components/imageCrop/index.tsx
+++ src/components/imageCrop/index.tsx
@@ -2,10 +2,104 @@ import * as React from 'react'
 
 import ReactCrop from 'react-image-crop'
 
-export default class ImageCrop extends React.Component<{}>{
+export default class ImageCrop extends React.Component {
+    imageRef = null
+
+    fileUrl = null
+    
+    state = {
+        src: '',
+        croppedImageUrl: null,
+        crop: {
+            aspect: 1,
+            width: 50,
+            x: 0,
+            y: 0,
+        },
+    };
+
+    onSelectFile = e => {
+        if (e.target.files && e.target.files.length > 0) {
+            const reader = new FileReader();
+            reader.addEventListener('load', () =>
+                this.setState({ src: reader.result }),
+            );
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    };
+
+    onImageLoaded = (image: HTMLImageElement) => {
+        this.imageRef = image;
+    };
+
+    onCropComplete = (crop, pixelCrop) => {
+        this.makeClientCrop(crop, pixelCrop);
+    };
+
+    onCropChange = crop => {
+        this.setState({ crop });
+    };
+
+    async makeClientCrop(crop, pixelCrop) {
+        if (this.imageRef && crop.width && crop.height) {
+            const croppedImageUrl = await this.getCroppedImg(
+                this.imageRef,
+                pixelCrop,
+                'newFile.jpeg',
+            );
+            this.setState({ croppedImageUrl });
+        }
+    }
+
+    getCroppedImg(image, pixelCrop, fileName) {
+        const canvas = document.createElement('canvas');
+        canvas.width = pixelCrop.width;
+        canvas.height = pixelCrop.height;
+        const ctx = canvas.getContext('2d');
+
+        ctx.drawImage(
+            image,
+            pixelCrop.x,
+            pixelCrop.y,
+            pixelCrop.width,
+            pixelCrop.height,
+            0,
+            0,
+            pixelCrop.width,
+            pixelCrop.height,
+        );
+
+        return new Promise((resolve, reject) => {
+            canvas.toBlob((blob: any) => {
+                blob.name = fileName;
+                window.URL.revokeObjectURL(this.fileUrl);
+                this.fileUrl = window.URL.createObjectURL(blob);
+                resolve(this.fileUrl);
+            }, 'image/jpeg');
+        });
+    }
+
     render() {
+        const { crop, croppedImageUrl, src } = this.state;
+
         return (
-            <div>11</div>
-        )
+            <div className="App">
+                <div>
+                    <input type="file" onChange={this.onSelectFile} />
+                </div>
+                {src && (
+                    <ReactCrop
+                        src={src}
+                        crop={crop}
+                        onImageLoaded={this.onImageLoaded}
+                        onComplete={this.onCropComplete}
+                        onChange={this.onCropChange}
+                    />
+                )}
+                {croppedImageUrl && (
+                    <img alt="Crop" style={{ maxWidth: '100%' }} src={croppedImageUrl} />
+                )}
+            </div>
+        );
     }
 }
