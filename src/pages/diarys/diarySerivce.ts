3bcb326505dc5dfafadd552758c7215cb5b73160
ReusLi <574399934@qq.com>
2019-01-08 22:35:26 +0800

    add diary service .ts

---
+++ src/pages/diarys/diarySerivce.ts
@@ -0,0 +1,28 @@
+import { DiaryVO } from 'interface/diary'
+
+import diaryStore from 'store/diary'
+
+import $http from 'utils/http'
+
+/**
+ * 根据日期查询日记
+ * @param date 日期
+ */
+export const findDiaryByDate = (date: string): Promise<Array<DiaryVO>> => {
+    return new Promise(async (resolve, reject) => {
+        const condition = {
+            where: {
+                createTime: {
+                    gte: `${date} 00:00:00`,
+                    lte: `${date} 23:59:59`
+                }
+            }
+        }
+        const result = await $http.post('/api/pyDiary/findAll', {
+            condition: condition
+        })
+        const data: Array<DiaryVO> = result.data
+        diaryStore.setDiaryList(data)
+        resolve(diaryStore.diaryList)
+    })
+}
