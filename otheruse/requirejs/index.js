8782e879033c2fc11a0980aa844de8b21951d98e
ReusLi <574399934@qq.com>
2019-02-22 23:06:22 +0800

    update code

--- otheruse/requirejs/index.js
+++ otheruse/requirejs/index.js
@@ -2,11 +2,28 @@ const fs = require('fs')
 const path = require('path')
 
 const babel = require('babel-core')
+const babeltraverse = require('babel-traverse')
+// traverse, Visitor
+const traverse = babeltraverse.default
+const visitors = babeltraverse.visitors
 
 const filePath = path.join(__dirname, 'source', 'test.js')
 const fileContent = fs.readFileSync(filePath, 'utf-8')
 
 const astNode = babel.transform(fileContent)
 
+traverse(astNode.ast, {
+    enter(path) {
+        if (path.isExpressionStatement()) {
+            console.log(path.node.start)
+        }
+        if (path.isIdentifier({ name: 'test' })) {
+            console.dir(path.node)
+        }
+        if (path.isFunction()) {
+            debugger
+        }
+    }
+})
 
 debugger
